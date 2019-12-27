9720a362a750149486f652fa98d6f0e3f3a6a6ac
vitalves <vitalvesilva@gmail.com>
2019-12-27 15:20:18 -0300

    layout login

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import Login from './pages/Login';
+
+export default function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
